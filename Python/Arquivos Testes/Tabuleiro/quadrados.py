c01d5e4105710aea433ef6082cda2b87956feb50
GuitHeck <48369275+GuitHeck@users.noreply.github.com>
2019-10-23 22:47:27 -0300

    Add files via upload

--- Python/Arquivos Testes/Tabuleiro/quadrados.py
+++ Python/Arquivos Testes/Tabuleiro/quadrados.py
@@ -18,81 +18,90 @@ def clicouTabuleiro (PosMouse, Superficie, PosSuperficie):
         #Rect(x, y, width, height)
         widthSuperficie = 90
         heightSuperficie = 72
-        print (PosMouse)
-        print (PosSuperficie)
         return PosMouse[0] >= PosSuperficie[0] and PosMouse[0] <= (PosSuperficie[0] + widthSuperficie) and PosMouse[1] >= PosSuperficie[1] and PosMouse[1] <= (PosSuperficie[1] +  heightSuperficie)
 
 
 
 direcao = True
 coordXTabuleiroEsquerda = 0
-coordXTabuleiroDireita= 72
 coordYTabuleiroCima = 648
-coordYTabuleiroBaixo = 720
 rodando = True
 contador = 0
-numSorteado = 100
+numSorteado = randint(1,7)
+contadorErros = 0
+localiza = True
 while rodando:
     cor = (255,0,0)
     dimensao = [90,72]
     coord =[coordXTabuleiroEsquerda,coordYTabuleiroCima]
-    if contador == numSorteado:
-        rodando = False
     if direcao:
         if contador != numSorteado:
             if coordXTabuleiroEsquerda != 810 :
-                pygame.draw.rect(screen,cor,Rect(coord,dimensao))
                 coordXTabuleiroEsquerda = coordXTabuleiroEsquerda+90
-                coordXTabuleiroDireita = coordXTabuleiroDireita+90
+                pygame.draw.rect(screen,cor,Rect(coord,dimensao))
                 pygame.time.wait(100)
                 contador +=1
                 if contador == numSorteado:
                     coordXTabuleiroEsquerda = coordXTabuleiroEsquerda-90
-                    coordXTabuleiroDireita = coordXTabuleiroDireita-90
             else:
                 direcao = False
                 coordYTabuleiroCima = coordYTabuleiroCima-72
-                coordYTabuleiroBaixo = coordYTabuleiroBaixo-72
                 pygame.draw.rect(screen,cor,Rect(coord,dimensao))
                 contador +=1
                 pygame.time.wait(100)
     else:
+        print ("Entrou else")
         if contador != numSorteado:
             if coordXTabuleiroEsquerda != 0:
-                pygame.draw.rect(screen,cor,Rect(coord,dimensao))
                 coordXTabuleiroEsquerda = coordXTabuleiroEsquerda-90
-                coordXTabuleiroDireita = coordXTabuleiroDireita-90
+                pygame.draw.rect(screen,cor,Rect(coord,dimensao))
                 contador +=1
                 if contador == numSorteado:
                     coordXTabuleiroEsquerda = coordXTabuleiroEsquerda+90
-                    coordXTabuleiroDireita = coordXTabuleiroDireita+90
                 pygame.time.wait(100)
             else:
                 direcao = True
                 coordYTabuleiroCima = coordYTabuleiroCima-72
-                coordYTabuleiroBaixo = coordYTabuleiroBaixo-72
                 pygame.draw.rect(screen,cor,Rect(coord,dimensao))
                 pygame.time.wait(100)
                 contador +=1
+    if contador %10 == 0:
+        retangulo = (coordXTabuleiroEsquerda,coordYTabuleiroCima+50,90,72)
+        posicao =  (coordXTabuleiroEsquerda,coordYTabuleiroCima+50)
+    else:
+        retangulo = (coordXTabuleiroEsquerda,coordYTabuleiroCima,90,72)
+        posicao =  (coordXTabuleiroEsquerda,coordYTabuleiroCima)
+    pygame.display.update()
+    localiza = True
+    if contador == numSorteado:
+        while localiza:
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    posMouse = event.pos       
+                    if contador == numSorteado:
+                        print (contadorErros)
+                        print (posMouse)
+                        print (posicao)
+                        if clicouTabuleiro(posMouse,retangulo,posicao ):
+                            print("Acertou:")
+                            contadorErros = 0
+                            localiza = False
+                            sorteio = randint(1,7)
+                            print (sorteio)
+                            numSorteado = numSorteado + sorteio
+                        else:
+                            print ("errou")
+                            contadorErros = contadorErros + 1
+                            if contadorErros >=5:
+                                cor = (0,255,0)
+                                pygame.draw.rect(screen,cor,Rect(coord,dimensao))
+                                pygame.display.update()
+    if contador == 100:
+        print("ganhou")
+        rodando = False
     pygame.display.update()
 
-localiza = True
-if contador %10 == 0:
-    retangulo = (coordXTabuleiroEsquerda,coordYTabuleiroCima+50,90,72)
-    posicao =  (coordXTabuleiroEsquerda,coordYTabuleiroCima+50)
-else:
-    retangulo = (coordXTabuleiroEsquerda,coordYTabuleiroCima,90,72)
-    posicao =  (coordXTabuleiroEsquerda,coordYTabuleiroCima)
-while localiza:
-    for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            posMouse = event.pos
-            if contador == numSorteado:
-                if clicouTabuleiro(posMouse,retangulo,posicao ):
-                    print("Acertou:")
-                    localiza = False
-                else:
-                    print ("errou")
+    
 
 
 
