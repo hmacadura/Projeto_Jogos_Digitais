5a22f7676b708bcb4b859d46badf37b37b9d77d3
GuitHeck <48369275+GuitHeck@users.noreply.github.com>
2019-10-28 18:52:02 -0300

    Implementado a parte dos teleportes

--- Python/Arquivos Testes/Tabuleiro/quadrados.py
+++ Python/Arquivos Testes/Tabuleiro/quadrados.py
@@ -6,6 +6,11 @@ from sys import exit
 ####
 pygame.init()
 
+
+fundoTelaTabuleiro = pygame.image.load("../../../Sprites/Fundo/tabuleiroComSimbolos.png")
+
+
+
 screen = pygame.display.set_mode((1280,720))
 for event in pygame.event.get():
     if event.type == QUIT:
@@ -26,14 +31,17 @@ direcao = True
 coordXTabuleiroEsquerda = 0
 coordYTabuleiroCima = 648
 rodando = True
-contador = 0
-numSorteado = randint(1,7)
+contador = 1
+numSorteado = 85
 contadorErros = 0
 localiza = True
 coord =[coordXTabuleiroEsquerda,coordYTabuleiroCima]
+posFundo = [0,0]
 while rodando:
     cor = (255,0,0)
     dimensao = [90,72]
+    pygame.draw.rect(screen,cor,Rect(coord,dimensao))
+    screen.blit(fundoTelaTabuleiro ,posFundo)
     if direcao:
         if contador != numSorteado:
             if coordXTabuleiroEsquerda != 810 :
@@ -50,9 +58,8 @@ while rodando:
                 contador +=1
                 pygame.time.wait(100)
     else:
-        print ("Entrou else")
         if contador != numSorteado:
-                if coordXTabuleiroEsquerda != 0:
+            if coordXTabuleiroEsquerda != 0:
                 coordXTabuleiroEsquerda = coordXTabuleiroEsquerda-90
                 coord =[coordXTabuleiroEsquerda,coordYTabuleiroCima]
                 pygame.draw.rect(screen,cor,Rect(coord,dimensao))
@@ -83,8 +90,56 @@ while rodando:
                             contadorErros = 0
                             localiza = False
                             sorteio = randint(1,7)
-                            print (sorteio)
                             numSorteado = numSorteado + sorteio
+                            print (contador)
+                            if contador== 4:
+                                coordXTabuleiroEsquerda = 630
+                                coordYTabuleiroCima = 504
+                                contador = 28
+                                numSorteado = 28
+                                coord =[coordXTabuleiroEsquerda,coordYTabuleiroCima]
+                            elif contador== 37:
+                                coordXTabuleiroEsquerda = 450
+                                coordYTabuleiroCima = 216
+                                contador = 66
+                                numSorteado = 66
+                                coord =[coordXTabuleiroEsquerda,coordYTabuleiroCima]
+                                direcao = True
+                            elif contador== 42:
+                                coordXTabuleiroEsquerda = 630
+                                coordYTabuleiroCima = 72
+                                contador = 88
+                                numSorteado = 88
+                                coord =[coordXTabuleiroEsquerda,coordYTabuleiroCima]
+                                direcao = True
+                            elif contador== 53:
+                                coordXTabuleiroEsquerda = 360
+                                coordYTabuleiroCima = 576
+                                contador = 16
+                                numSorteado = 16
+                                coord =[coordXTabuleiroEsquerda,coordYTabuleiroCima]
+                                direcao = False
+                                pygame.display.update()
+                            elif contador== 63:
+                                coordXTabuleiroEsquerda = 450
+                                coordYTabuleiroCima = 432
+                                contador = 43
+                                numSorteado = 43
+                                coord =[coordXTabuleiroEsquerda,coordYTabuleiroCima]
+                                direcao = False
+                                pygame.display.update()
+                            elif contador== 85:
+                                coordXTabuleiroEsquerda = 180
+                                coordYTabuleiroCima = 360
+                                contador = 43
+                                numSorteado = 43
+                                coord =[coordXTabuleiroEsquerda,coordYTabuleiroCima]
+                                direcao = True
+                                pygame.display.update()
+                            
+                            
+                            
+                                
                         else:
                             print ("errou")
                             contadorErros = contadorErros + 1
